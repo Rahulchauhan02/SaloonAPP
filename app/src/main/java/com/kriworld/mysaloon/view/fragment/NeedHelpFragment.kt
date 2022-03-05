6369ce2d379ad79a4c8080ad1c21bd3c447fdc42
Rahul chauhan <rahulchauhan8461@gmail.com>
2022-03-05 17:21:14 +0530

    Saloon-App

--- app/src/main/java/com/kriworld/mysaloon/view/fragment/NeedHelpFragment.kt
+++ app/src/main/java/com/kriworld/mysaloon/view/fragment/NeedHelpFragment.kt
@@ -5,20 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.kriworld.mysaloon.R
+import com.kriworld.mysaloon.databinding.FragmentNeedHelpBinding
 
 
 class NeedHelpFragment : Fragment() {
 
-
+  private lateinit var needHelpBinding: FragmentNeedHelpBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_need_help, container, false)
+        needHelpBinding = FragmentNeedHelpBinding.inflate(layoutInflater)
+
+        needHelpBinding.backNeed.setOnClickListener {
+            findNavController().navigate(R.id.action_needHelpFragment_to_profileFragment)
+        }
+
+
+        return needHelpBinding.root
     }
 
 
